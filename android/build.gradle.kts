07f8809b3cb04589e99c374f2cc8859166a5aed0
Ahmad Raza <ahmadrazabiz01@gmail.com>
2025-05-06 18:13:17 +0500

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,18 +2,23 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-    }  
+    }
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    // Skip Flutter plugins to avoid path conflicts
+    if (!project.name.contains("google_sign_in") && 
+        !project.name.contains("flutter")) {
+        val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+        project.layout.buildDirectory.value(newSubprojectBuildDir)
+    }
 }
+
 subprojects {
-    project.evaluationDependsOn(":app")
+    project.evaluationDependsOn(":app")  
 }
 
 tasks.register<Delete>("clean") {
